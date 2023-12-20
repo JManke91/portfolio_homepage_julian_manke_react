a3fe1e039c9934e5304bff862fb5c959597b7c31
JManke91 <Julian.Manke91@gmail.com>
2023-12-20 10:11:27 +0200

    animation logic for overlay bug fixing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,9 +6,26 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isTransitionEnabled, setIsTransitionEnabled] = useState(false);
 
+  // ASYNC OPERATION
   const toggleMenu = () => {
-    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+    //setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+
+    if (!menuOpen) {
+      setIsTransitionEnabled(true);
+      setMenuOpen(true);
+      console.log('isTransitionEnabled from toggleMenu:', isTransitionEnabled);
+    } else {
+      //setIsTransitionEnabled(true);
+      setMenuOpen(false);
+    }
+  };
+
+  const onAnimationEnd = () => {
+    // FIXME: NEVER BEING CALLED
+    console.log('onAnimationEnd called');
+    setIsTransitionEnabled(false);
   };
 
   return (
@@ -19,7 +36,11 @@ function Header() {
           <span></span>
           <span></span>
         </div>
-        <nav className={`nav-links ${menuOpen ? 'open' : ''}`}>
+        <nav
+          onAnimationEnd={onAnimationEnd}
+          className={`nav-links ${menuOpen ? 'open' : 'close'} ${isTransitionEnabled ? 'transition-enabled' : 'no-transition'
+            }`}
+        >
           <ul>
             <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
             <li><Link to="/portfolio" onClick={() => setMenuOpen(false)}>Portfolio</Link></li>
@@ -32,3 +53,4 @@ function Header() {
 }
 
 export default Header;
+
